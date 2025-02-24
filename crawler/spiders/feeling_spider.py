715e4d69d04894811d658b137eacb3537710fe93
Wisdom@yesulikplimits <wwwanyizah@gmail.com>
2025-02-24 04:55:01 +0000

    New update

--- crawler/spiders/feeling_spider.py
+++ crawler/spiders/feeling_spider.py
@@ -1,26 +1,48 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
+import json
+from datetime import datetime
 
 class FeelingSpider(scrapy.Spider):
     name = "feeling_spider"
-    start_urls = [
-        "https://www.facebook.com/posts",  # Replace with real URLs
-    ]
+    
+    # Multi-platform crawling
+    custom_settings = {
+        'CONCURRENT_REQUESTS': 4,
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'output.json'
+    }
 
-    def parse(self, response):
-        for post in response.css("div.post"):
+    def start_requests(self):
+        # Blog platforms
+        platforms = [
+            'https://medium.com/tag/emotions/archive/',
+            'https://www.blogger.com/search?q=I%20feel'
+        ]
+        for url in platforms:
+            yield scrapy.Request(url=url, callback=self.parse_blog)
+
+        # Twitter API integration
+        yield scrapy.Request(
+            url="https://api.twitter.com/2/tweets/search/recent?query=I%20feel",
+            headers={"Authorization": f"Bearer {self.settings.get('TWITTER_BEARER_TOKEN')}"},
+            callback=self.parse_tweets
+        )
+
+    def parse_blog(self, response):
+        for post in response.css('article.post'):
             yield {
-                "url": post.css("a::attr(href)").get(),
-                "content": post.css("p::text").getall(),
-                "author": post.css("span.author::text").get(),
-                "date": post.css("span.date::text").get()
+                'content': post.css('div.post-content ::text').getall(),
+                'author': post.css('span.author-name ::text').get(),
+                'date': datetime.now().isoformat(),
+                'platform': 'blog'
             }
 
-# Run the crawler (test)
-if __name__ == "__main__":
-    process = CrawlerProcess(settings={
-        "FEED_FORMAT": "json",
-        "FEED_URI": "output.json"
-    })
-    process.crawl(FeelingSpider)
-    process.start()
+    def parse_tweets(self, response):
+        data = json.loads(response.text)
+        for tweet in data.get('data', []):
+            yield {
+                'content': [tweet['text']],
+                'author': tweet['author_id'],
+                'date': tweet['created_at'],
+                'platform': 'twitter'
+            }
